538718d5271a511665868aa30c4ffebe3149ed28
AzeemAzamTarar <93082652+AzeemAzamTarar@users.noreply.github.com>
2025-04-03 13:41:38 +0500

    Add files via upload

---
+++ Challenge 9/solution.sql
@@ -0,0 +1,31 @@
+CREATE TABLE monthly_sales (
+month DATE,
+product_id INTEGER,
+amount_sold INTEGER
+);
+
+INSERT INTO monthly_sales (month, product_id, amount_sold) VALUES
+('2021-01-01', 1, 100),
+('2021-01-01', 2, 300),
+('2021-02-01', 1, 150),
+('2021-02-01', 1, 50),
+('2021-02-01', 2, 250),
+('2021-03-01', 1, 120),
+('2021-03-01', 4, 250),
+('2021-04-01', 2, -30),
+('2021-04-01', 3, 200),
+('2021-05-01', 3, 175),
+('2021-06-01', 1, 0),
+('2021-06-01', 2, 100);
+
+-- Do not modify the schema or data definitions above
+
+-- Implement your SQL query below, utilizing the provided schema
+
+select month,
+  Sum( Case when product_id = '1' then amount_sold else 0 end) as product_1,
+  Sum( Case when product_id = '2' then amount_sold else 0 end) as product_2,
+  Sum( Case when product_id = '3' then amount_sold else 0 end) as product_3,
+  Sum( Case when product_id = '4' then amount_sold else 0 end) as product_4
+from monthly_sales
+group by month
